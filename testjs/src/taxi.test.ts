3453212fa1abefd63075956d635b6c8258cdfec7
Ploypatcharin Poolsavatkitikool <ploypatcharin.p@arise.tech>
2024-05-15 10:56:00 +0700

    Implement Taxi function

--- testjs/src/taxi.test.ts
+++ testjs/src/taxi.test.ts
@@ -1,8 +1,20 @@
 import {Taxi} from './taxi'
 
 describe('Taxi', () => {
-  it('example test', () => {
-    const result = Taxi()
-    expect(result).toEqual(55)
+  it('test kilo not rounded up and minutes rounded up', () => {
+    const result = Taxi(1.5, 1.1)
+    expect(result).toEqual(6)
+  })
+  it('test kilo and minutes rounded up', () => {
+    const result = Taxi(1.6, 2.9)
+    expect(result).toEqual(11)
+  })
+  it('test kilo and minutes not rounded up', () => {
+    const result = Taxi(1.49, 1)
+    expect(result).toEqual(5)
+  })
+  it('test kilo rounded up and minutes not rounded up', () => {
+    const result = Taxi(1.51, 1)
+    expect(result).toEqual(9)
   })
 })
--- testjs/src/taxi.ts
+++ testjs/src/taxi.ts
@@ -1,1 +1,13 @@
-export const Taxi = () => 55
+export const Taxi = (kilo :number, minutes :number) => {
+    var sec = (minutes * 60) % 60
+    var min = Math.floor(minutes)
+    if(sec > 0){
+        min += 1
+    }
+    var k = kilo % 1
+    kilo = Math.floor(kilo)
+    if(k > 0.5){
+        kilo += 1
+    }
+    return 4 * kilo + 1*min
+}
