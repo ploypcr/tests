a159bc6fc993d6ea525ddcfae275c3c15f8bf26f
Ploypatcharin Poolsavatkitikool <ploypatcharin.p@arise.tech>
2024-05-17 13:25:17 +0700

    #2 Pass test case should return Fizz when input can mod by 3

--- testgo/fizzbuzz.go
+++ testgo/fizzbuzz.go
@@ -2,14 +2,19 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
+var boolMap = map[bool]int{false: 0, true: 1} 
 func FizzBuzz(n int) string {
-	s := ToString(n)
+	s := Fizz(n)
+	if(len(s) == 0){
+		s = ToString(n)
+	}
 	return s
 }
 func Fizz(n int) string{
-	return "Fizz"
+	return strings.Repeat("Fizz", boolMap[n%3==0])
 }
 func Buzz(n int) string{
 	return "Buzz"
