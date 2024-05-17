78b1f3bef3325304449c92640f0311d52a389982
Ploypatcharin Poolsavatkitikool <ploypatcharin.p@arise.tech>
2024-05-17 13:27:13 +0700

    #2 Pass test case should return Buzz when input can mod by 5 and FizzBuzz when input can mod by 3 and 5

--- testgo/fizzbuzz.go
+++ testgo/fizzbuzz.go
@@ -8,6 +8,7 @@ import (
 var boolMap = map[bool]int{false: 0, true: 1} 
 func FizzBuzz(n int) string {
 	s := Fizz(n)
+	s += Buzz(n)
 	if(len(s) == 0){
 		s = ToString(n)
 	}
@@ -17,7 +18,7 @@ func Fizz(n int) string{
 	return strings.Repeat("Fizz", boolMap[n%3==0])
 }
 func Buzz(n int) string{
-	return "Buzz"
+	return strings.Repeat("Buzz", boolMap[n%5==0])
 }
 func ToString(n int) string{
 	return fmt.Sprintf("%v", n)
