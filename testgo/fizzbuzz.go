3088261fd802c62662e18afddaf9e0d495216d30
Ploypatcharin Poolsavatkitikool <ploypatcharin.p@arise.tech>
2024-05-17 13:45:18 +0700

    #3 Modified ToString function ,Pass all test case

--- testgo/fizzbuzz.go
+++ testgo/fizzbuzz.go
@@ -2,27 +2,23 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
+var boolMap = map[bool]int{false: 0, true: 1} 
 func FizzBuzz(n int) string {
-	if (n% 3 == 0 && n%5 == 0){
-		return Fizz(n) + Buzz(n)
-	}
-	if (n % 3 == 0){
-		return Fizz(n)
-	}
-	if (n % 5 == 0){
-		return Buzz(n)
-	}
-
-	return ToString(n)
+	s := Fizz(n)
+	s += Buzz(n)
+	s += ToString(n)
+	return s
 }
 func Fizz(n int) string{
-	return "Fizz"
+	return strings.Repeat("Fizz", boolMap[n%3==0])
 }
 func Buzz(n int) string{
-	return "Buzz"
+	return strings.Repeat("Buzz", boolMap[n%5==0])
 }
 func ToString(n int) string{
-	return fmt.Sprintf("%v", n)
+	var res = fmt.Sprintf("%v", n)
+	return strings.Repeat(res, boolMap[n%3 != 0 && n%5 != 0])
 }
