b4f958b232820344391062f61861f94212469cf7
Ploypatcharin Poolsavatkitikool <ploypatcharin.p@arise.tech>
2024-05-23 15:56:38 +0700

    #4 Pass all test case

--- testgo/fizzbuzz.go
+++ testgo/fizzbuzz.go
@@ -6,18 +6,19 @@ import (
 )
 
 var boolMap = map[bool]int{false: 0, true: 1} 
+
 func FizzBuzz(n int) string {
-	s := Fizz(n)
-	s += Buzz(n)
-	s += ToString(n)
-	return s
+	return Fizz(n) + Buzz(n) + ToString(n)
 }
+
 func Fizz(n int) string{
 	return strings.Repeat("Fizz", boolMap[n%3==0])
 }
+
 func Buzz(n int) string{
 	return strings.Repeat("Buzz", boolMap[n%5==0])
 }
+
 func ToString(n int) string{
 	var res = fmt.Sprintf("%v", n)
 	return strings.Repeat(res, boolMap[n%3 != 0 && n%5 != 0])
